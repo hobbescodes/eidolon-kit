6136afeede93d1ae2b2a5b18779643c41d7df62a
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-14 03:14:16 -0500

    Feat: Lint Staged (#7)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@eidolonkit/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -104,4 +104,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
