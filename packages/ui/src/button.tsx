7ada655965d5745fd1c1e06ffe23275552217c68
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-14 00:44:49 -0500

    feat(ui): add support for biome

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, type = "button", appName, ...rest }: ButtonProps) => {
   return (
     <button
+      type={type}
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
+      {...rest}
     >
       {children}
     </button>
