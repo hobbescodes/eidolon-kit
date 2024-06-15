a48aca41a952b424db8b3207c551d551623db821
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-14 20:12:10 -0500

    feat(env): add base for blockchain env config

---
+++ packages/env/src/blockchain.ts
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production", "test"]).optional(),
+    FORK_RPC_URL: z.string(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+});
