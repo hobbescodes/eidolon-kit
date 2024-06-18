78332e23922c63c45a22eea7cc3045838b73c770
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-18 18:21:55 -0500

    refactor(vocs): update configuration

--- apps/web/vocs.config.ts
+++ apps/web/vocs.config.ts
@@ -2,5 +2,14 @@ import { defineConfig } from "vocs";
 
 export default defineConfig({
   description: "This is the documentation site for the Web App",
-  title: "Web App Documentation",
+  title: "Web App",
+  sidebar: [
+    {
+      text: "Getting Started",
+      link: "/",
+    },
+  ],
+  theme: {
+    accentColor: "#86ead4",
+  },
 });
