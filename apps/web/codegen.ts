78a8cc9a9fe76d63f047ff7a0b4d17a4638f70e7
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-20 12:08:45 -0500

    build(web): add gql.tada and codegen dependencies

---
+++ apps/web/codegen.ts
@@ -0,0 +1,17 @@
+import { env } from "@eidolonkit/env/web";
+
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: env.NEXT_PUBLIC_PONDER_API_URL,
+  generates: {
+    "./src/generated/schema.graphql": {
+      plugins: ["schema-ast"],
+      config: {
+        commentDescriptions: true,
+      },
+    },
+  },
+};
+
+export default config;
