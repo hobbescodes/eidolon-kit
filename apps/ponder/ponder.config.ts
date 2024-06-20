5a2a2ac45e305ce8fe319696cf58564f549d78d5
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-20 09:30:14 -0500

    refactor(contracts): update exports pattern

--- apps/ponder/ponder.config.ts
+++ apps/ponder/ponder.config.ts
@@ -1,14 +1,9 @@
 import { createConfig } from "@ponder/core";
-import { http, getAddress, hexToNumber } from "viem";
+import { http } from "viem";
 
-import anvilBroadcast from "@eidolonkit/contracts/anvil";
+import { contractAddress, startBlock } from "@eidolonkit/contracts/anvil";
 import { counterAbi } from "@eidolonkit/contracts/wagmi";
 import { env } from "@eidolonkit/env/blockchain";
-
-const address = getAddress(anvilBroadcast.transactions[0]!.contractAddress);
-const startBlock = hexToNumber(
-  anvilBroadcast.receipts[0]!.blockNumber as `0x${string}`,
-);
 
 export default createConfig({
   networks: {
@@ -22,7 +17,7 @@ export default createConfig({
     Counter: {
       network: "anvil",
       abi: counterAbi,
-      address,
+      address: contractAddress,
       startBlock,
       includeCallTraces: true,
     },
