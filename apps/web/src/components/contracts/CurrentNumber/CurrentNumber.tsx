e1ab2566e8b6c81022715acf7ce214308b2ea837
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-20 13:29:01 -0500

    refactor(web): use NumberInput, add Spinner

--- apps/web/src/components/contracts/CurrentNumber/CurrentNumber.tsx
+++ apps/web/src/components/contracts/CurrentNumber/CurrentNumber.tsx
@@ -1,20 +1,36 @@
 "use client";
 
+import { Spinner } from "components/core";
 import { useCurrentNumber } from "lib/hooks";
 
 const CurrentNumber = () => {
   const { data: number, isLoading, isError } = useCurrentNumber();
 
-  if (isLoading) return <p>Loading...</p>;
-
-  if (isError) return <p>Error fetching current number</p>;
+  if (isError)
+    return (
+      <p className="text-red-600 font-semibold">
+        Error fetching current number
+      </p>
+    );
 
   return (
     <div className="flex flex-col gap-4 items-center">
-      <p className="text-lg font-semibold">Current number:</p>
-      <p className="text-xl font-semibold text-accent-text px-4 py-3 bg-[var(--colors-accent-3)] rounded-md border border-accent-text">
-        {Number(number)}
-      </p>
+      {isError ? (
+        <p className="text-red-600 font-semibold">
+          Error fetching current number
+        </p>
+      ) : (
+        <>
+          <p className="text-lg font-semibold">Current number:</p>
+          <div className="flex items-center text-xl font-semibold p-4 text-accent-text bg-[var(--colors-accent-3)] rounded-md border border-accent-text">
+            {isLoading ? (
+              <Spinner className="h-7 w-7" />
+            ) : (
+              <p className="w-7 text-center">{Number(number)}</p>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- apps/web/src/components/contracts/SetNumber/SetNumber.tsx
+++ apps/web/src/components/contracts/SetNumber/SetNumber.tsx
@@ -2,17 +2,22 @@
 
 import { useAccount } from "wagmi";
 
-import { Button, Input } from "@eidolonkit/ui";
+import { Button, NumberInput } from "@eidolonkit/ui";
 
 const SetNumber = () => {
   const { isConnected } = useAccount();
 
   return (
     <div className="flex items-center gap-2 max-w-xs">
-      <Input
+      <NumberInput
         id="number"
-        placeholder="Enter a number..."
         className="flex-1"
+        placeholder="Set number..."
+        allowOverflow={false}
+        min={0}
+        formatOptions={{
+          maximumFractionDigits: 0,
+        }}
         disabled={!isConnected}
       />
       <Button disabled={!isConnected}>Set</Button>
