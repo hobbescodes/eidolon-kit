1dce7d15f1b006cc116b4c07ed1f1bdcfe223ed0
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-06-20 09:30:47 -0500

    chore(contracts): add line spacing for anvil.ts

--- packages/contracts/generated/anvil.ts
+++ packages/contracts/generated/anvil.ts
@@ -5,6 +5,7 @@ import anvilBroadcast from "../broadcast/Deploy.s.sol/31337/run-latest.json";
 export const contractAddress = getAddress(
   anvilBroadcast.transactions[0]?.contractAddress ?? zeroAddress,
 );
+
 export const startBlock = hexToNumber(
   (anvilBroadcast.receipts[0]?.blockNumber as `0x${string}`) ?? "0x0",
 );
